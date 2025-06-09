51f9a524bf67e3cff333db15d179b5a203b4f79a
Godmode_ON <150623220+ConsoleSystemPrint@users.noreply.github.com>
2025-06-09 19:25:10 +0500

    9

--- src/view-v/edit-form.js
+++ src/view-v/edit-form.js
@@ -27,7 +27,7 @@ export default class Filter {
   }
 }
 
-export function createEditForm(point, destinations, offersByType) {
+export function createEditForm(point, destinations, offersByType) { // 1
   const typeOptions = Object.keys(offersByType).map((type) =>
     `<option value="${type}" ${point.type === type ? 'selected' : ''}>${type}</option>`
   ).join('');
@@ -39,7 +39,7 @@ export function createEditForm(point, destinations, offersByType) {
   const offerCheckboxes = offersForType.map((offer) =>
     `<label>
       <input type="checkbox" name="offers" value="${offer.id}" ${point.offers.includes(offer.id) ? 'checked' : ''}>
-      ${offer.title} (+€${offer.price})
+      ${offer.title} (+€${offer.price}) 
     </label>`
   ).join('');
 
